49b189c027ed089998421672c4a2e084064654f7
Piti Ongmongkolkul <piti118@gmail.com>
2013-01-21 10:19:56 -0800

    move website

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     long_description=''.join(open('README.rst').readlines()[4:]),
     author='Piti Ongmongkolkul',
     author_email='piti118@gmail.com',
-    url='https://github.com/piti118/dist_fit',
+    url='https://github.com/iminuit/probfit',
     package_dir={'probfit': 'probfit'},
     packages=['probfit'],
     ext_modules=[costfunc, pdf, libstat, funcutil, functor],
