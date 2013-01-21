bfd53dcb9609ef82ab00e0eb1e47d41de9063f26
Piti Ongmongkolkul <piti118@gmail.com>
2013-01-21 11:46:57 -0800

    and update corresponding .py file

--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -3,27 +3,25 @@
 
 # <headingcell level=1>
 
-# dist_fit Basic Tutorial
+# probfit Basic Tutorial
 
-# <codecell>
+# <markdowncell>
 
-from probfit import *
-import numpy as np
-import iminuit
-import UserDict
-from math import sqrt, exp, pi
-
-# <headingcell level=2>
-
-# Lets start with a simple simple straight line
-
-# <rawcell>
-
+# ##Lets start with a simple simple straight line
+# 
 # We can't really call this a fitting package without being able to fit a straight line, right?
 
 # <codecell>
 
-#lets make s straight line with gaussian(mu=0,sigma=1) error
+from probfit import describe, Chi2Regression
+import iminuit
+import numpy as np
+import numpy.random as npr
+
+# <codecell>
+
+#lets make s straight line with gaussian(mu=0,sigma=1) noise
+npr.seed(0)
 x = linspace(0,10,20) 
 y = 3*x+15+ randn(20)
 err = np.array([1]*20)
@@ -58,9 +56,9 @@ describe(chi2)
 #minimize it
 #yes it gives you a heads up that you didn't give it initial value
 #we can ignore it for now
-minimizer = RTMinuit.Minuit(chi2) #see RTMinuit tutorial on how to give initial value/range/error
+minimizer = iminuit.Minuit(chi2) #see iminuit tutorial on how to give initial value/range/error
 minimizer.migrad(); #very stable robust minimizer
-#you can look at your terminal to see what it is doing
+#you can look at your terminal to see what it is doing;
 
 # <codecell>
 
@@ -71,17 +69,16 @@ print minimizer.errors
 # <codecell>
 
 #or a pretty printing
-minimizer.html_results()
-#you can also call display(minimizer.html_results()) if this is in the middle of your code
+minimizer.print_fmin()
 
-# <rawcell>
+# <markdowncell>
 
-# Parabolic error
+# ####Parabolic error
 # is calculated using the second derivative at the minimum
 # This is good in most cases where the uncertainty is symmetric not much correlation
 # exists. Migrad usually got this accurately but if you want ot be sure
 # call minimizer.hesse() after migrad
-# Minos Error
+# ####Minos Error
 # is obtained by scanning chi^2 and likelihood profile and find the the point
 # where chi^2 is increased by 1 or -ln likelihood is increased by 0.5
 # This error is generally asymmetric and is correct in all case.
@@ -97,29 +94,33 @@ chi2.draw(minimizer)
 # <codecell>
 
 #Sometime we want the error matrix
-print 'error matrix:\n', minimizer.error_matrix()
+print 'error matrix:\n', minimizer.matrix()
 #or correlation matrix
-print 'correlation matrix:\n', minimizer.error_matrix(correlation=True)
+print 'correlation matrix:\n', minimizer.matrix(correlation=True)
 #or a pretty html
 #despite the method named error_matrix it's actually a correlation matrix
-display(minimizer.html_error_matrix())
+minimizer.print_matrix()
 
-# <headingcell level=2>
+# <markdowncell>
 
-# Simple gaussian distribution fit
-
-# <rawcell>
-
+# ##Simple gaussian distribution fit
 # In high energy physics, we usually want to fit a distribution to a histogram. Let's look at simple gaussian distribution
 
 # <codecell>
 
+import numpy.random as npr
+import iminuit
+from probfit import BinnedLH
+
+# <codecell>
+
 #First lets make our data
+npr.seed(0)
 data = randn(10000)*4+1
 #sigma = 4 and mean = 1
 hist(data,bins=100,histtype='step');
 
-# <rawcell>
+# <markdowncell>
 
 # Here is our PDF/model
 
@@ -137,7 +138,7 @@ cost = BinnedLH(myPDF,data)
 # <codecell>
 
 #Let's fit
-minimizer = RTMinuit.Minuit(cost,sigma=3.) #notice here that we give initial value to sigma
+minimizer = iminuit.Minuit(cost,sigma=3.) #notice here that we give initial value to sigma
 
 #up parameter determine where it determine uncertainty
 #1(default) for chi^2 and 0.5 for likelihood
@@ -155,10 +156,10 @@ print 'Error:', minimizer.errors
 # <codecell>
 
 #That printout can get out of hand quickly
-display(minimizer.html_results())
+minimizer.print_fmin()
 #and correlation matrix
 #will not display well in firefox(tell them to fix writing-mode:)
-display(minimizer.html_error_matrix()) 
+minimizer.print_matrix()
 
 # <codecell>
 
@@ -168,7 +169,7 @@ cost.draw(minimizer)#uncertainty is given by symetric poisson
 # <codecell>
 
 #how about making sure the error making sense
-draw_contour(cost,minimizer,'mu')
+minimizer.draw_mnprofile('mu');
 
 # <codecell>
 
@@ -176,23 +177,28 @@ draw_contour(cost,minimizer,'mu')
 #you can notice that it takes sometime to draw
 #we will this is because our PDF is defined in Python
 #we will show how to speed this up later
-draw_contour2d(cost,minimizer,'mu','sigma');
+minimizer.draw_mncontour('mu','sigma');
 
-# <headingcell level=2>
+# <markdowncell>
 
-# How about Chi^2
+# ##How about Chi^2
+# 
+# Let's explore another popular cost function chi^2. Chi^2 is bad when you have bin with 0. ROOT just ignore. ROOFIT does something I don't remember. But's it's best to avoid using chi^2 when you have bin with 0 count.
 
-# <rawcell>
+# <codecell>
 
-# Let's explore another popular cost function chi^2
+import numpy.random as npr
+from math import sqrt,exp,pi
+import iminuit
+from probfit import Extended, gaussian, BinnedChi2, describe
 
 # <codecell>
 
 #we will use the same data as in the previous example
-np.random.seed(0)
-data = randn(10000)*4+1
+npr.seed(0)
+data = npr.randn(10000)*4+1
 #sigma = 4 and mean = 1
-hist(data,bins=100,histtype='step');
+hist(data, bins=100, histtype='step');
 
 # <codecell>
 
@@ -203,7 +209,7 @@ def myPDF(x,mu,sigma):
 # <codecell>
 
 #binned chi^2 fit only makes sense for extended fit
-extended_pdf = Extend(myPDF)
+extended_pdf = Extended(myPDF)
 
 # <codecell>
 
@@ -215,8 +221,8 @@ describe(extended_pdf) #you can look at what your pdf means
 #Chi^2 distribution fit is really bad for distribution with long tail
 #since when bin count=0... poisson error=0 and blows up chi^2
 #so give it some range
-chi2 = BinnedChi2(extended_pdf,data,range=(-7,10))
-minimizer = RTMinuit.Minuit(chi2,sigma=1)
+chi2 = BinnedChi2(extended_pdf,data,bound=(-7,10))
+minimizer = iminuit.Minuit(chi2,sigma=1)
 minimizer.migrad()
 
 # <codecell>
@@ -226,23 +232,27 @@ chi2.draw(minimizer)
 # <codecell>
 
 #and usual deal
-display(minimizer.html_results())
-display(minimizer.html_error_matrix()) 
+minimizer.print_fmin()
+minimizer.print_matrix() 
 
-# <headingcell level=2>
+# <markdowncell>
 
-# Unbinned Likelihood and How to speed things up
-
-# <rawcell>
-
+# ##Unbinned Likelihood and How to speed things up
+# 
 # Unbinned likelihood is computationally very very expensive.
 # It's now a good time that we talk about how to speed things up with cython
 
 # <codecell>
 
+import numpy.random as npr
+from probfit import UnbinnedLH, gaussian, describe
+import iminuit
+
+# <codecell>
+
 #same data
-np.random.seed(0)
-data = randn(10000)*4+1
+npr.seed(0)
+data = npr.randn(10000)*4+1
 #sigma = 4 and mean = 1
 hist(data,bins=100,histtype='step');
 
@@ -266,46 +276,52 @@ def cython_PDF(double x,double mu,double sigma):
 # <codecell>
 
 #cost function 
-ublh = UnbinnedML(cython_PDF,data)
-minimizer = RTMinuit.Minuit(ublh,sigma=2.)
+ublh = UnbinnedLH(cython_PDF,data)
+minimizer = iminuit.Minuit(ublh,sigma=2.)
 minimizer.set_up(0.5)#remember this is likelihood
 minimizer.migrad()#yes amazingly fast
 ublh.show(minimizer)
-display(minimizer.html_results())
-display(minimizer.html_error_matrix()) 
+minimizer.print_fmin()
+minimizer.print_matrix() 
 
 # <codecell>
 
 #remember how slow it was?
 #now it's super fast(and it's even unbinned likelihood)
-draw_contour(ublh,minimizer,'mu')
+minimizer.draw_mnprofile('mu');
+
+# <markdowncell>
+
+# But you really don't have to write your own gaussian there are tons of builtin function written in cython for you.
 
 # <codecell>
 
-#but you really don't have to write your own gaussian 
-#there are tons of builtin function written in cython for you
+import probfit.pdf
+print dir(probfit.pdf)
 print describe(gaussian)
 print type(gaussian)
 
 # <codecell>
 
-ublh = UnbinnedML(gaussian,data)
-minimizer = RTMinuit.Minuit(ublh,sigma=2.)
+ublh = UnbinnedLH(gaussian,data)
+minimizer = iminuit.Minuit(ublh,sigma=2.)
 minimizer.set_up(0.5)#remember this is likelihood
 minimizer.migrad()#yes amazingly fast
 ublh.show(minimizer)
-display(minimizer.html_results())
-display(minimizer.html_error_matrix()) 
 
-# <headingcell level=2>
+# <markdowncell>
 
-# But... We can't normalize everything analytically and how to generate toy sample from PDF
-
-# <rawcell>
-
+# ##But... We can't normalize everything analytically and how to generate toy sample from PDF
+# 
 # When fitting distribution to a PDF. One of the common problem that we run into is normalization.
 # Not all function is analytically integrable on the range of our interest.
-# Let's look at crystal ball function
+# Let's look at crystal ball function.
+
+# <codecell>
+
+from probfit import crystalball, gen_toy, Normalized, describe, UnbinnedLH
+import numpy.random as npr
+import iminuit
 
 # <codecell>
 
@@ -321,7 +337,7 @@ describe(crystalball)
 
 # <codecell>
 
-np.random.seed(0)
+npr.seed(0)
 bound = (-1,2)
 data = gen_toy(crystalball,10000,bound=bound,alpha=1.,n=2.,mean=1.,sigma=0.3,quiet=False)
 #quiet = False tells it to plot out original function
@@ -331,8 +347,8 @@ data = gen_toy(crystalball,10000,bound=bound,alpha=1.,n=2.,mean=1.,sigma=0.3,qui
 
 #To fit this we need to tell normalized our crystal ball PDF
 #this is done with trapezoid rule with simple cache mechanism
-#can be done by Normalize functor
-ncball = Normalize(crystalball,bound)
+#can be done by Normalized functor
+ncball = Normalized(crystalball,bound)
 #this can also bedone with declerator
 #@normalized_function(bound)
 #def myPDF(x,blah):
@@ -348,28 +364,14 @@ describe(ncball)
 # <codecell>
 
 #now we can fit as usual
-ublh = UnbinnedML(ncball,data)
-minimizer = RTMinuit.Minuit(ublh,
+ublh = UnbinnedLH(ncball,data)
+minimizer = iminuit.Minuit(ublh,
     alpha=1.,n=2.1,mean=1.2,sigma=0.3)
 minimizer.set_up(0.5)#remember this is likelihood
 minimizer.migrad()#yes amazingly fast Normalize is written in cython
 ublh.show(minimizer)
 #crystalball function is nortorious for its sensitivity on n parameter
 #dist_fit give you a heads up where it might have float overflow
-
-# <rawcell>
-
-# Bonus: one thing we normally ask about asymmetric peaking distribution like this is full-width-half-max
-# http://en.wikipedia.org/wiki/Full_width_at_half_maximum
-
-# <codecell>
-
-lw,rw = fwhm_f(ncball,(-1,2),minimizer.args)
-
-# <codecell>
-
-ublh.draw(minimizer)
-axvspan(lw,rw,alpha=0.2,color='green');
 
 # <headingcell level=2>
 
@@ -379,10 +381,10 @@ axvspan(lw,rw,alpha=0.2,color='green');
 
 #crystalball is nortoriously sensitive to initial parameter
 #now it is a good time to show what happen when things...doesn't fit
-ublh = UnbinnedML(ncball,data)
-minimizer = RTMinuit.Minuit(ublh)#NO initial value
+ublh = UnbinnedLH(ncball,data)
+minimizer = iminuit.Minuit(ublh)#NO initial value
 minimizer.set_up(0.5)#remember this is likelihood
-minimizer.migrad()#yes amazingly fast
+minimizer.migrad()#yes amazingly fast but tons of read
 #Remember there is a heads up
 
 # <codecell>
@@ -396,22 +398,30 @@ minimizer.migrad_ok(), minimizer.matrix_accurate()
 
 # <codecell>
 
-#fix it by giving it initial value/error/limit or fixing parameter see RTMinuit Tutorial
+#fix it by giving it initial value/error/limit or fixing parameter see iminuit Tutorial
 #now we can fit as usual
-ublh = UnbinnedML(ncball,data)
-minimizer = RTMinuit.Minuit(ublh,
+ublh = UnbinnedLH(ncball,data)
+minimizer = iminuit.Minuit(ublh,
     alpha=1.,n=2.1,mean=1.2,sigma=0.3)
 minimizer.set_up(0.5)#remember this is likelihood
 minimizer.migrad()#yes amazingly fast. Normalize is written in cython
 ublh.show(minimizer)
 
-# <headingcell level=2>
+# <markdowncell>
 
-# How do we guess initial value?
-
-# <rawcell>
-
+# ##How do we guess initial value?
+# 
 # This is a hard question but visualization can helps us
+
+# <codecell>
+
+from probfit import try_uml, Normalized, crystalball, gen_toy
+
+# <codecell>
+
+bound = (-1,2)
+ncball = Normalized(crystalball,bound)
+data = gen_toy(crystalball,10000,bound=bound,alpha=1.,n=2.,mean=1.,sigma=0.3)
 
 # <codecell>
 
@@ -430,24 +440,35 @@ print besttry #and you can find which one give you minimal unbinned likelihood
 
 # <codecell>
 
-peak1 = randn(3000)*0.2
-peak2 = randn(5000)*0.1+4
+import numpy.random as npr
+from probfit import gen_toy, gaussian, Extended, describe, try_binlh, BinnedLH
+import numpy as np
+import iminuit
+
+# <codecell>
+
+peak1 = npr.randn(3000)*0.2
+peak2 = npr.randn(5000)*0.1+4
 bg = gen_toy(lambda x : (x+2)**2, 20000,(-2,5))
 all_data = np.concatenate([peak1,peak2,bg])
 hist((peak1,peak2,bg,all_data),bins=200,histtype='step',range=(-2,5));
 
 # <codecell>
 
+%load_ext cythonmagic
+
+# <codecell>
+
 %%cython
 cimport cython
-from dist_fit import Normalize, gaussian
+from probfit import Normalized, gaussian
 
 @cython.binding(True)
 def poly(double x,double a,double b, double c):
     return a*x*x+b*x+c
 
 #remember linear function is not normalizable for -inf ... +inf
-nlin = Normalize(poly,(-1,5))
+nlin = Normalized(poly,(-1,5))
 
 #our extended PDF for 3 types of signal
 @cython.binding(True)
@@ -478,16 +499,16 @@ print besttry
 
 # <codecell>
 
-binlh = BinnedLH(myPDF,all_data, bins=200, extended=True, range=(-1,5))
+binlh = BinnedLH(myPDF,all_data, bins=200, extended=True, bound=(-1,5))
 #too lazy to type initial values from what we try?
 #use keyword expansion **besttry
 #need to put in initial step size for mu1 and mu2 
 #with error_mu* otherwise it won't converge(try it yourself)
-minimizer = RTMinuit.Minuit(binlh, error_mu1=0.1, error_mu2=0.1, **besttry)
+minimizer = iminuit.Minuit(binlh, error_mu1=0.1, error_mu2=0.1, **besttry)
 
 # <codecell>
 
-minimizer.migrad()
+minimizer.migrad();
 
 # <codecell>
 
@@ -495,15 +516,12 @@ binlh.show(minimizer)
 
 # <codecell>
 
-display(minimizer.html_results())
-minimizer.html_error_matrix()
+minimizer.print_fmin()
 
-# <headingcell level=2>
+# <markdowncell>
 
-# Advance: Custom cost function and Simultaneous Fit
-
-# <rawcell>
-
+# ##Advance: Custom cost function and Simultaneous Fit
+# 
 # Sometimes, what we want to fit is the sum of likelihood /chi^2 of two PDF sharing some parameters.
 # dist_fit doesn't provied a built_in facility to do this but it can be built easily.
 # 
@@ -512,18 +530,24 @@ minimizer.html_error_matrix()
 
 # <codecell>
 
-np.random.seed(0)
+import numpy.random as npr
+from probfit import UnbinnedLH, gaussian, describe, draw_compare_hist
+import iminuit
+
+# <codecell>
+
+npr.seed(0)
 #Lets make two gaussian
-data1 = randn(10000)+3
-data2 = randn(10000)-2
+data1 = npr.randn(10000)+3
+data2 = npr.randn(10000)-2
 hist([data1,data2],bins=100,histtype='step',label=['data1','data2']);
 
 # <codecell>
 
 #remember this is nothing special about builtin cost function
 #except some utility function like draw and show
-ulh1 = UnbinnedML(gaussian,data1)
-ulh2 = UnbinnedML(gaussian,data2)
+ulh1 = UnbinnedLH(gaussian,data1)
+ulh2 = UnbinnedLH(gaussian,data2)
 print describe(ulh1)
 print describe(ulh2)
 
@@ -532,8 +556,8 @@ print describe(ulh2)
 #you can also use cython to do this
 class CustomCost:
     def __init__(self,pdf1,data1,pdf2,data2):
-        self.ulh1 = UnbinnedML(pdf1,data1)
-        self.ulh2 = UnbinnedML(pdf2,data2)
+        self.ulh1 = UnbinnedLH(pdf1,data1)
+        self.ulh2 = UnbinnedLH(pdf2,data2)
     #this is the important part you need __call__ to calculate your cost
     #in our case it's sum of likelihood with sigma
     def __call__(self,mu1,mu2,sigma):
@@ -545,18 +569,21 @@ simul_lh = CustomCost(gaussian,data1,gaussian,data2)
 
 # <codecell>
 
-minimizer = RTMinuit.Minuit(simul_lh,sigma=0.5)
+minimizer = iminuit.Minuit(simul_lh,sigma=0.5)
 minimizer.set_up(0.5)#remember it's likelihood
-minimizer.migrad()
+minimizer.migrad();
 
 # <codecell>
 
-display(minimizer.html_results())
-display(minimizer.html_error_matrix())
+minimizer.print_fmin()
+minimizer.print_matrix()
 results = minimizer.values
 
 # <codecell>
 
 draw_compare_hist(gaussian,[results['mu1'],results['sigma']],data1,normed=True);
 draw_compare_hist(gaussian,[results['mu2'],results['sigma']],data2,normed=True);
+
+# <codecell>
+
 
