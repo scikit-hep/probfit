da6e49fcb4e4158507dcb966cd393f02c7938e25
Christoph Deil <Deil.Christoph@gmail.com>
2013-05-16 13:53:45 +0200

    Clean up tutorial a bit
    - no import *
    - code formatting

--- test/testfit.py
+++ test/testfit.py
@@ -1,95 +1,93 @@
-import unittest
-from probfit.costfunc import UnbinnedLH, BinnedLH, BinnedChi2, Chi2Regression,\
-                             SimultaneousFit
-from probfit.pdf import gaussian, linear
-from numpy.random import randn, seed
-from math import log,pi,sqrt
+import unittest 
 import warnings
-from iminuit.iminuit_warnings import InitialParamWarning
-from probfit.util import describe
-from nose.tools import *
-from probfit.funcutil import rename
+from nose.tools import assert_equal
 import numpy as np
-from iminuit import Minuit
+import iminuit
+from iminuit import describe
+from iminuit.iminuit_warnings import InitialParamWarning
+from probfit.funcutil import rename
+from probfit.pdf import gaussian, linear
+from probfit.costfunc import UnbinnedLH, BinnedLH, BinnedChi2, Chi2Regression, \
+                             SimultaneousFit
 
-def assert_almost_equal(x,y,delta=1e-7):
-    if y-delta < x < y+delta:
+def assert_almost_equal(x, y, delta=1e-7):
+    if y - delta < x < y + delta:
         pass
     else:
-        raise AssertionError('x = %f and y = %f differs more than %g'%(x,y,delta))
+        raise AssertionError('x = %f and y = %f differs more than %g' % (x, y, delta))
 
 class TestFit(unittest.TestCase):
 
     def setUp(self):
         warnings.simplefilter("ignore", InitialParamWarning)
-        seed(0)
+        np.random.seed(0)
         self.ndata = 20000
-        self.data = randn(self.ndata)
-        self.analytic = self.ndata*0.5*(log(2*pi)+1)
+        self.data = np.random.randn(self.ndata)
+        self.analytic = self.ndata * 0.5 * (np.log(2 * np.pi) + 1)
 
     def test_UnbinnedLH(self):
         f = gaussian
-        assert_equal(list(describe(f)), ['x','mean','sigma'])
+        assert_equal(list(describe(f)), ['x', 'mean', 'sigma'])
         lh = UnbinnedLH(gaussian, self.data,)
-        assert_equal(list(describe(lh)), ['mean','sigma'])
-        assert_almost_equal(lh(0,1), 28188.201229348757)
-        minuit = Minuit(lh)
+        assert_equal(list(describe(lh)), ['mean', 'sigma'])
+        assert_almost_equal(lh(0, 1), 28188.201229348757)
+        minuit = iminuit.Minuit(lh)
         assert_equal(minuit.errordef, 0.5)
 
     def test_BinnedLH(self):
-        #write a better test... this depends on subtraction
+        # write a better test... this depends on subtraction
         f = gaussian
-        assert_equal(list(describe(f)), ['x','mean','sigma'])
-        lh = BinnedLH(gaussian, self.data, bound=[-3,3])
-        assert_equal(list(describe(lh)), ['mean','sigma'])
-        assert_almost_equal(lh(0,1), 20.446130781601543, 1)
-        minuit = Minuit(lh)
+        assert_equal(list(describe(f)), ['x', 'mean', 'sigma'])
+        lh = BinnedLH(gaussian, self.data, bound=[-3, 3])
+        assert_equal(list(describe(lh)), ['mean', 'sigma'])
+        assert_almost_equal(lh(0, 1), 20.446130781601543, 1)
+        minuit = iminuit.Minuit(lh)
         assert_equal(minuit.errordef, 0.5)
 
 
     def test_BinnedChi2(self):
         f = gaussian
-        assert_equal(list(describe(f)), ['x','mean','sigma'])
-        lh = BinnedChi2(gaussian, self.data, bound=[-3,3])
-        assert_equal(list(describe(lh)), ['mean','sigma'])
-        assert_almost_equal(lh(0,1), 19951.005399882044, 1)
-        minuit = Minuit(lh)
+        assert_equal(list(describe(f)), ['x', 'mean', 'sigma'])
+        lh = BinnedChi2(gaussian, self.data, bound=[-3, 3])
+        assert_equal(list(describe(lh)), ['mean', 'sigma'])
+        assert_almost_equal(lh(0, 1), 19951.005399882044, 1)
+        minuit = iminuit.Minuit(lh)
         assert_equal(minuit.errordef, 1.0)
 
 
     def test_Chi2Regression(self):
         x = np.linspace(1, 10, 10)
-        y = 10*x+1
+        y = 10 * x + 1
         f = linear
-        assert_equal(list(describe(f)), ['x','m','c'])
+        assert_equal(list(describe(f)), ['x', 'm', 'c'])
 
         lh = Chi2Regression(f, x, y)
 
-        assert_equal(list(describe(lh)), ['m','c'])
+        assert_equal(list(describe(lh)), ['m', 'c'])
 
         assert_almost_equal(lh(10, 1), 0)
 
         assert_almost_equal(lh(10, 0), 10.)
-        minuit = Minuit(lh)
+        minuit = iminuit.Minuit(lh)
         assert_equal(minuit.errordef, 1.0)
 
 
     def test_simultaneous(self):
-        seed(0)
-        data = randn(10000)
-        shifted = data+3.
+        np.random.seed(0)
+        data = np.random.randn(10000)
+        shifted = data + 3.
         g1 = rename(gaussian, ['x', 'lmu', 'sigma'])
         g2 = rename(gaussian, ['x', 'rmu', 'sigma'])
-        ulh1 = UnbinnedLH(g1,data)
-        ulh2 = UnbinnedLH(g2,shifted)
+        ulh1 = UnbinnedLH(g1, data)
+        ulh2 = UnbinnedLH(g2, shifted)
         sim = SimultaneousFit(ulh1, ulh2)
-        assert_equal(describe(sim),['lmu', 'sigma', 'rmu'])
-        minuit = Minuit(sim,sigma=1.2, pedantic=False, print_level=0)
+        assert_equal(describe(sim), ['lmu', 'sigma', 'rmu'])
+        minuit = iminuit.Minuit(sim, sigma=1.2, pedantic=False, print_level=0)
         minuit.migrad()
         assert(minuit.migrad_ok())
-        assert_almost_equal(m.values['lmu'], 0., delta=2 * minuit.errors['lmu'])
-        assert_almost_equal(m.values['rmu'], 3., delta=2 * minuit.errors['rmu'])
-        assert_almost_equal(m.values['sigma'], 1., delta=2 * minuit.errors['sigma'])
+        assert_almost_equal(minuit.values['lmu'], 0., delta=2 * minuit.errors['lmu'])
+        assert_almost_equal(minuit.values['rmu'], 3., delta=2 * minuit.errors['rmu'])
+        assert_almost_equal(minuit.values['sigma'], 1., delta=2 * minuit.errors['sigma'])
 
 if __name__ == '__main__':
     unittest.main()
--- test/testoneshot.py
+++ test/testoneshot.py
@@ -1,126 +1,121 @@
 import unittest
-from iminuit import Minuit
-from probfit import *
-from probfit.pdf import  gaussian
-from probfit.functor import Extended
-from probfit.oneshot import fit_binlh, fit_binx2, fit_uml
-import numpy as np
-from numpy.random import randn, seed
-from math import sqrt
-from iminuit.iminuit_warnings import InitialParamWarning
 import warnings
+from math import sqrt
+import numpy as np
+from iminuit import Minuit
+from iminuit.iminuit_warnings import InitialParamWarning
+from probfit import Normalized, Extended, UnbinnedLH
+from probfit.pdf import  gaussian
+from probfit.oneshot import fit_binlh, fit_binx2, fit_uml
 
-def assert_almost_equal(x,y,delta=1e-7):
-    if y-delta < x < y+delta:
+def assert_almost_equal(x, y, delta=1e-7):
+    if y - delta < x < y + delta:
         pass
     else:
-        raise AssertionError('%10f and %10f differ more than %e'%(x,y,delta))
+        raise AssertionError('%10f and %10f differ more than %e' % (x, y, delta))
 
 class TestOneshot(unittest.TestCase):
 
     def setUp(self):
         self.ndata = 20000
         warnings.simplefilter("ignore", InitialParamWarning)
-        seed(0)
-        self.data = randn(self.ndata)*2. + 5.
+        np.random.seed(0)
+        self.data = np.random.randn(self.ndata) * 2. + 5.
         self.wdown = np.empty(self.ndata)
         self.wdown.fill(0.1)
 
         self.ndata_small = 2000
-        self.data_small = randn(self.ndata_small)*2. + 5.
+        self.data_small = np.random.randn(self.ndata_small) * 2. + 5.
 
     def test_binx2(self):
         egauss = Extended(gaussian)
-        fit,m = fit_binx2(egauss, self.data, bins=100, bound=(1.,9.),
+        _, minuit = fit_binx2(egauss, self.data, bins=100, bound=(1., 9.),
             quiet=True, mean=4., sigma=1., N=10000., print_level=0)
-        assert(m.migrad_ok())
-        assert_almost_equal(m.values['mean'], 5., delta=3*m.errors['mean'])
-        assert_almost_equal(m.values['sigma'], 2., delta=3*m.errors['sigma'])
+        assert(minuit.migrad_ok())
+        assert_almost_equal(minuit.values['mean'], 5., delta=3 * minuit.errors['mean'])
+        assert_almost_equal(minuit.values['sigma'], 2., delta=3 * minuit.errors['sigma'])
 
     def test_binlh(self):
-        ngauss=Normalized(gaussian,(1.,9.))
-        fit,m = fit_binlh(ngauss, self.data,bins=100, bound=(1.,9.), quiet=True,
+        ngauss = Normalized(gaussian, (1., 9.))
+        _, minuit = fit_binlh(ngauss, self.data, bins=100, bound=(1., 9.), quiet=True,
             mean=4., sigma=1.5, print_level=0)
-        assert(m.migrad_ok())
-        assert_almost_equal(m.values['mean'],5.,delta=3*m.errors['mean'])
-        assert_almost_equal(m.values['sigma'],2.,delta=3*m.errors['sigma'])
+        assert(minuit.migrad_ok())
+        assert_almost_equal(minuit.values['mean'], 5., delta=3 * minuit.errors['mean'])
+        assert_almost_equal(minuit.values['sigma'], 2., delta=3 * minuit.errors['sigma'])
 
     def test_extended_binlh(self):
         egauss = Extended(gaussian)
-        fit,m = fit_binlh(egauss,self.data, bins=100, bound=(1.,9.), quiet=True,
+        _, minuit = fit_binlh(egauss, self.data, bins=100, bound=(1., 9.), quiet=True,
             mean=4., sigma=1., N=10000.,
             print_level=0, extended=True)
-        assert(m.migrad_ok())
-        assert_almost_equal(m.values['mean'],5.,delta=3*m.errors['mean'])
-        assert_almost_equal(m.values['sigma'],2.,delta=3*m.errors['sigma'])
-        assert_almost_equal(m.values['N'],20000,delta=3*m.errors['N'])
+        assert(minuit.migrad_ok())
+        assert_almost_equal(minuit.values['mean'], 5., delta=3 * minuit.errors['mean'])
+        assert_almost_equal(minuit.values['sigma'], 2., delta=3 * minuit.errors['sigma'])
+        assert_almost_equal(minuit.values['N'], 20000, delta=3 * minuit.errors['N'])
 
     def test_extended_binlh_ww(self):
         egauss = Extended(gaussian)
-        fit,m = fit_binlh(egauss,self.data,bins=100, bound=(1.,9.), quiet=True,
-            mean=4., sigma=1.,N=1000., weights=self.wdown,
+        _, minuit = fit_binlh(egauss, self.data, bins=100, bound=(1., 9.), quiet=True,
+            mean=4., sigma=1., N=1000., weights=self.wdown,
             print_level=0, extended=True)
-        assert(m.migrad_ok())
-        assert_almost_equal(m.values['mean'],5.,delta=m.errors['mean'])
-        assert_almost_equal(m.values['sigma'],2.,delta=m.errors['sigma'])
-        assert_almost_equal(m.values['N'],2000,delta=m.errors['N'])
+        assert(minuit.migrad_ok())
+        assert_almost_equal(minuit.values['mean'], 5., delta=minuit.errors['mean'])
+        assert_almost_equal(minuit.values['sigma'], 2., delta=minuit.errors['sigma'])
+        assert_almost_equal(minuit.values['N'], 2000, delta=minuit.errors['N'])
 
     def test_extended_binlh_ww_w2(self):
         egauss = Extended(gaussian)
-        fit,m = fit_binlh(egauss,self.data,bins=100, bound=(1.,9.), quiet=True,
-            mean=4., sigma=1.,N=1000., weights=self.wdown,
+        _, minuit = fit_binlh(egauss, self.data, bins=100, bound=(1., 9.), quiet=True,
+            mean=4., sigma=1., N=1000., weights=self.wdown,
             print_level=0, extended=True)
-        assert_almost_equal(m.values['mean'],5.,delta=m.errors['mean'])
-        assert_almost_equal(m.values['sigma'],2.,delta=m.errors['sigma'])
-        assert_almost_equal(m.values['N'],2000,delta=m.errors['N'])
-        assert(m.migrad_ok())
+        assert_almost_equal(minuit.values['mean'], 5., delta=minuit.errors['mean'])
+        assert_almost_equal(minuit.values['sigma'], 2., delta=minuit.errors['sigma'])
+        assert_almost_equal(minuit.values['N'], 2000, delta=minuit.errors['N'])
+        assert(minuit.migrad_ok())
 
-        fit2,m2 = fit_binlh(egauss,self.data,bins=100, bound=(1.,9.), quiet=True,
-            mean=4., sigma=1.,N=1000., weights=self.wdown,
-            print_level=-1, extended=True, use_w2=True)
-        assert_almost_equal(m2.values['mean'],5.,delta=2*m2.errors['mean'])
-        assert_almost_equal(m2.values['sigma'],2.,delta=2*m2.errors['sigma'])
-        assert_almost_equal(m2.values['N'], 2000., delta=2*m2.errors['N'])
-        assert(m2.migrad_ok())
+        _, minuit2 = fit_binlh(egauss, self.data, bins=100, bound=(1., 9.), quiet=True,
+            mean=4., sigma=1., N=1000., weights=self.wdown,
+            print_level= -1, extended=True, use_w2=True)
+        assert_almost_equal(minuit2.values['mean'], 5., delta=2 * minuit2.errors['mean'])
+        assert_almost_equal(minuit2.values['sigma'], 2., delta=2 * minuit2.errors['sigma'])
+        assert_almost_equal(minuit2.values['N'], 2000., delta=2 * minuit2.errors['N'])
+        assert(minuit2.migrad_ok())
 
-        m.minos()
-        m2.minos()
+        minuit.minos()
+        minuit2.minos()
 
-        #now error should scale correctly
-        assert_almost_equal( m.errors['mean']/sqrt(10),
-                                m2.errors['mean'],
-                                delta = m.errors['mean']/sqrt(10)/100.)
-        assert_almost_equal(m.errors['sigma']/sqrt(10),
-                               m2.errors['sigma'],
-                               delta = m.errors['sigma']/sqrt(10)/100.)
-        assert_almost_equal(m.errors['N']/sqrt(10),
-                               m2.errors['N'],
-                               delta = m.errors['N']/sqrt(10)/100.)
+        # now error should scale correctly
+        assert_almost_equal(minuit.errors['mean'] / sqrt(10),
+                            minuit2.errors['mean'],
+                            delta=minuit.errors['mean'] / sqrt(10) / 100.)
+        assert_almost_equal(minuit.errors['sigma'] / sqrt(10),
+                            minuit2.errors['sigma'],
+                            delta=minuit.errors['sigma'] / sqrt(10) / 100.)
+        assert_almost_equal(minuit.errors['N'] / sqrt(10),
+                            minuit2.errors['N'],
+                            delta=minuit.errors['N'] / sqrt(10) / 100.)
 
     def test_uml(self):
-        fit,m = fit_uml(gaussian, self.data, quiet=True,
-                        mean=4.5, sigma=1.5, print_level=0)
-        assert(m.migrad_ok())
-        assert_almost_equal(m.values['mean'],5.,delta=3*m.errors['mean'])
-        assert_almost_equal(m.values['sigma'],2.,delta=3*m.errors['sigma'])
+        _, minuit = fit_uml(gaussian, self.data, quiet=True,
+                         mean=4.5, sigma=1.5, print_level=0)
+        assert(minuit.migrad_ok())
+        assert_almost_equal(minuit.values['mean'], 5., delta=3 * minuit.errors['mean'])
+        assert_almost_equal(minuit.values['sigma'], 2., delta=3 * minuit.errors['sigma'])
 
     def test_extended_ulh(self):
         eg = Extended(gaussian)
-        lh = UnbinnedLH(eg, self.data, extended=True, extended_bound=(-20,20))
-        minuit = Minuit(lh, mean=4.5, sigma=1.5, N=19000., 
+        lh = UnbinnedLH(eg, self.data, extended=True, extended_bound=(-20, 20))
+        minuit = Minuit(lh, mean=4.5, sigma=1.5, N=19000.,
                         pedantic=False, print_level=0)
         minuit.migrad()
-        assert_almost_equal(minuit.values['N'],20000, delta=sqrt(20000.))
+        assert_almost_equal(minuit.values['N'], 20000, delta=sqrt(20000.))
         assert(minuit.migrad_ok())
 
     def test_extended_ulh_2(self):
         eg = Extended(gaussian)
         lh = UnbinnedLH(eg, self.data, extended=True)
-        minuit = Minuit(lh, mean=4.5, sigma=1.5, N=19000., 
+        minuit = Minuit(lh, mean=4.5, sigma=1.5, N=19000.,
                         pedantic=False, print_level=0)
         minuit.migrad()
         assert(minuit.migrad_ok())
-        assert_almost_equal(minuit.values['N'],20000, delta=sqrt(20000.))
-
-if __name__ == '__main__':
-    unittest.main()
+        assert_almost_equal(minuit.values['N'], 20000, delta=sqrt(20000.))
