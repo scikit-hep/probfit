e11dbeee614e1ed8cd61967eb0a7d585da701ce1
Alex Pearce <alex@alexpearce.me>
2016-11-11 14:27:47 +0100

    Allow setup.py execution without numpy.
    So that, for example, egg_info can be run. Adds some logging to tell the
    user if/when they’re missing a package.
    
    Closes #69.

--- setup.py
+++ setup.py
@@ -1,30 +1,47 @@
 from glob import glob
+import logging
 import os
 from setuptools import setup
 from setuptools.extension import Extension
 
-import numpy as np
+logging.basicConfig()
+log = logging.getLogger('probfit')
 
 try:
-    from Cython.Build import cythonize
+    import numpy as np
 
-    USE_CYTHON = True
+    USE_NUMPY = True
 except ImportError:
-    print('Cython is not available; using pre-generated C files')
-    USE_CYTHON = False
+    log.warning('Could not import numpy; C extensions will not be built')
+    USE_NUMPY = False
 
-ext = '.pyx' if USE_CYTHON else '.c'
 extensions = []
-for source_file in glob('probfit/*' + ext):
-    fname, _ = os.path.splitext(os.path.basename(source_file))
-    extensions.append(
-        Extension('probfit.{0}'.format(fname),
-                  sources=['probfit/{0}{1}'.format(fname, ext)],
-                  include_dirs=[np.get_include()])
-    )
+if USE_NUMPY:
+    try:
+        from Cython.Build import cythonize
 
-if USE_CYTHON:
-    extensions = cythonize(extensions)
+        USE_CYTHON = True
+    except ImportError:
+        log.warning('Cython is not available; using pre-generated C files')
+        USE_CYTHON = False
+
+    ext = '.pyx' if USE_CYTHON else '.c'
+    for source_file in glob('probfit/*' + ext):
+        print(source_file)
+        fname, _ = os.path.splitext(os.path.basename(source_file))
+        extensions.append(
+            Extension('probfit.{0}'.format(fname),
+                      sources=['probfit/{0}{1}'.format(fname, ext)],
+                      include_dirs=[np.get_include()])
+        )
+
+    if not extensions:
+        # If we get here, the user has numpy installed but there are no .c
+        # files to build, so they must be generated which requires Cython
+        log.error('Could not build extensions; you must install Cython')
+
+    if USE_CYTHON:
+        extensions = cythonize(extensions)
 
 
 def get_version():
