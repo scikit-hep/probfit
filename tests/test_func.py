39c09c0cd77a13ed77115990effb3bb5a74c43e9
andreas01060 <andreas.bally@gmail.com>
2018-05-19 17:34:55 +0200

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -66,7 +66,15 @@ def test_crystalball():
     assert_allclose(pdf.crystalball(14, 1, 2, 10, 2), 0.1353352832366127)
     assert_allclose(pdf.crystalball(6, 1, 2, 10, 2), 0.26956918209450376)
 
-
+# cpdef double doubecrystalball(double x,double alpha,double alpha2, double n,double n2, double mean,double sigma)
+def test_doublecrystalball():
+    assert describe(pdf.doublecrystalball) == ['x', 'alpha', 'alpha2', 'n', 'n2', 'mean', 'sigma']
+    assert_allclose(pdf.doublecrystalball(10, 1, 1, 2, 2, 10, 2), 1.)
+    assert_allclose(pdf.doublecrystalball(11, 1, 1, 2, 2, 10, 2), 0.8824969025845955)
+    assert_allclose(pdf.doublecrystalball(12, 1, 1, 2, 2, 10, 2), 0.6065306597126334)
+    assert_allclose(pdf.doublecrystalball(14, 1, 1, 2, 2, 10, 2), 0.1353352832366127)
+    assert_allclose(pdf.doublecrystalball(6, 1, 1, 2, 2, 10, 2), 0.26956918209450376)
+    
 # cpdef double argus(double x, double c, double chi, double p)
 def test_argus():
     assert describe(pdf.argus) == ['x', 'c', 'chi', 'p']
