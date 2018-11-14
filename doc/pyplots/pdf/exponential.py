787fb7203219da388c56422897a3316d788c3387
Matthieu Marinangeli <matthieu.marinangeli@epfl.ch>
2018-11-14 11:48:22 +0100

    update doc/pyplots/pdf/exponential.pdf with lambda instead of tau

--- doc/pyplots/pdf/exponential.py
+++ doc/pyplots/pdf/exponential.py
@@ -1,19 +1,18 @@
 from probfit.pdf import exponential
 from probfit.plotting import draw_pdf
-from collections import OrderedDict
 import matplotlib.pyplot as plt
 
 bound = (0, 5)
 
-arg = OrderedDict(tau=0.5)
+arg = {"lambda": 0.5}
 draw_pdf(exponential, arg=arg, bound=bound, label=str(arg), density=False,
          bins=100)
 
-arg = OrderedDict(tau=1.0)
+arg = {"lambda": 1.0}
 draw_pdf(exponential, arg=arg, bound=bound, label=str(arg), density=False,
          bins=100)
 
-arg = OrderedDict(tau=1.5)
+arg = {"lambda": 1.5}
 draw_pdf(exponential, arg=arg, bound=bound, label=str(arg), density=False,
          bins=100)
 
